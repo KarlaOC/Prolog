9c866321ab864adba73b7fd9fb4d6ac97a1248b1
KarlaOC <karla.orozco.chavez@gmail.com>
2016-10-28 14:52:57 -0500

    Listas imprimir/lista inversa/union listas

--- Listas.pl
+++ Listas.pl
@@ -1,11 +1,21 @@
-%Listas
-
+%Lista
 [x,y,z].
 
+%primer elemento
 p_e([C|R]):- write(C).
 
+%imprimiento lista
 imprimir_lista([C|R]):- write(C),nl, imprimir_lista(R).
 imprimir_lista([]).
+
+%imprimiendo lista inversa
+imprimir_inversa([C | R]):-imprimir_inversa(R),nl, write(C).
+imprimir_inversa([]).
+
+%concatenando dos listas
+union_listas([], Cs, Cs).
+union_listas([A|As],Bs,[A|Cs]):-union_listas(As, Bs, Cs). 
+
 
 
 

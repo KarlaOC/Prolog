78bbb36089038642551ba29009a4a60e3b6833c8
Karla <Karla.orozco.chavez@gmail.com>
2016-10-28 00:05:42 -0500

    imprimir primer elemento,imprimir lista

--- Listas.pl
+++ Listas.pl
@@ -7,3 +7,10 @@ p_e([C|R]):- write(C).
 imprimir_lista([C|R]):- write(C),nl, imprimir_lista(R).
 imprimir_lista([]).
 
+
+imprimir_inversa([C|R]):- write(R),nl, imprimir_inversa(R).
+imprimir_inversa([]).
+
+
+
+

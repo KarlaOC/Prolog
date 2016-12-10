cb7d458f478d3fdb3bc1adf7c53e37889f3d1385
KarlaOC <karla.orozco.chavez@gmail.com>
2016-12-09 20:17:14 -0600

    Create tipsbasic.go

---
+++ tipsbasic.go
@@ -0,0 +1,17 @@
+import (
+    "fmt"
+    "math/cmplx"
+)
+
+var (
+    ToBe   bool       = false
+    MaxInt uint64     = 1«64 - 1
+    z      complex128 = cmplx.Sqrt(-5 + 12i)
+)
+
+func main() {
+    const f = "%T(%v)\n"
+    fmt.Printf(f, ToBe, ToBe)
+    fmt.Printf(f, MaxInt, MaxInt)
+    fmt.Printf(f, z, z)
+}
